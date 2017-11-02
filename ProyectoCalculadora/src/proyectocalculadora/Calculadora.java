3fb8929dc930dd01fa1d5d6056ed33b961f09271
karyliz <karenlizettecabrera@gmail.com>
2017-11-02 17:29:38 -0300

    Un poco del menú

--- ProyectoCalculadora/src/proyectocalculadora/Calculadora.java
+++ ProyectoCalculadora/src/proyectocalculadora/Calculadora.java
@@ -1,6 +1,11 @@
 package proyectocalculadora;
+
 import java.util.*;
 
 public class Calculadora {
-    
+
+    int numeroUno = 5;
+    int numeroDos = 7;
+
+
 }
--- ProyectoCalculadora/src/proyectocalculadora/ProyectoCalculadora.java
+++ ProyectoCalculadora/src/proyectocalculadora/ProyectoCalculadora.java
@@ -1,10 +1,32 @@
 package proyectocalculadora;
+
 import java.util.*;
 
 public class ProyectoCalculadora {
 
-    public static void main(String[] args) {
+    public List<Integer> numeros;
 
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int cantidadNumeros;
+        int opcion;
+        int valores = 10;
+        
+        System.out.println("Bienvenido");
+        System.out.println("Cuantos numeros desea ingresar: ");
+        cantidadNumeros = leer.nextInt();
+        System.out.println("1° Sumar");
+        System.out.println("2° Restar");
+        System.out.println("Opcion: ");
+        opcion = leer.nextInt();
+        
+        switch(opcion){
+            case 1:
+                break;
+            case 2:
+                break;
+
+        }
     }
-    
+
 }
